4192c61ffee7611bac8ebfa68c034c29757ff0a2
Marc Mouries <marc@mouries.net>
2020-05-07 09:31:52 -0400

    create table + d3Toolkit

---
+++ mm_d3Toolkit.js
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+
+
+
+var mm_d3Toolkit = {
+
+
+  link_diagonal: function (d) {
+    return "M" + d.source.y + "," + d.source.x +
+      "C" + (d.source.y + d.target.y) / 2 + "," + d.source.x +
+      " " + (d.source.y + d.target.y) / 2 + "," + d.target.x +
+      " " + d.target.y + "," + d.target.x;
+  },
+
+
+
+  calcLeft: function (d) {
+    var l = d.y;
+    if (d.position === 'left') {
+      l = (d.y) - w / 2;
+      l = (w / 2) + l;
+    }
+    return {
+      x: d.x,
+      y: l
+    };
+  },
+
+
+  link_elbow: function (d, i) {
+    var source = mm_d3Toolkit.calcLeft(d.source);
+    var target = mm_d3Toolkit.calcLeft(d.target);
+    var hy = (target.y - source.y) / 2;
+    return "M" + source.y + "," + source.x +
+      "H" + (source.y + hy) +
+      "V" + target.x + "H" + target.y;
+  }
+
+
+
+}
---
+++ tableToolkit.js
@@ -0,0 +1,103 @@
+/**
+ * 
+ */
+var tableToolkit = {
+
+	/**
+	 * Create a tableless container for the data specified
+	 * 
+	 * @param {*} data contains the data to be presented in the form
+	 * @param {*} fields_header 
+	 * @param {*} fields_content 
+	 * @param {*} fields_footer 
+	 * @returns Document
+	 */
+	createTable: function (data, fields_header, fields_content, fields_footer) {
+
+		var html = "<table>";
+
+		var nb_cols_in_table = 4;
+		// HEADER
+		var print_field_name = false;
+		html += this._printFields(data, fields_header, "thead",  print_field_name, nb_cols_in_table);
+
+		// FIELDS
+		print_field_name = true;
+		html += this._printFields(data, fields_content, "tbody", print_field_name, nb_cols_in_table);
+
+		// FOOTER
+		html += this._printFields(data, fields_footer, "tfoot", print_field_name, nb_cols_in_table);
+
+		html += "</table>";
+		return html;
+	},
+
+
+	// print the list of fields from the data in the container specified
+	_printFields: function (data, field_list, container, print_field_name, nb_cols_in_table) {
+		var html = "";
+		html += "<" + container + ">";
+
+		var number_of_fields = field_list.length;
+		var number_of_col = (print_field_name ? 2 * number_of_fields : number_of_fields);
+		var number_of_row = Math.round(number_of_col /nb_cols_in_table );
+		// there is at leat one row
+		if (number_of_row <1) { number_of_row ++;}
+		var number_of_field_per_row = Math.round(number_of_fields / number_of_row);
+
+		var field_data_id = 1;
+		var row_number = 1;
+		for (;row_number <= number_of_row; row_number++) {
+			//console.log(" ROW " + row_number + "/" + number_of_row);
+			var column_start = "";
+			var column_end = "";
+			if (container == "thead") {
+				column_start = "<th";
+				column_end = "</th>";
+			} else {
+				column_start = "<td";
+				column_end = "</td>";
+			}
+			html += "<tr>";
+
+			var nb_col_printed = 1;
+			for (var field_number=1; field_number <= number_of_field_per_row; field_number++) {
+				// no more data
+				if (field_data_id > number_of_fields) {
+					continue;
+				}
+
+				var field = field_list[field_data_id-1];
+				//console.log( "   FIELD " + field_data_id  + "/" + number_of_fields);
+
+				var field_name = field;
+				if (print_field_name) {
+					html += column_start + ">" + field_name + column_end;
+					//console.log("     col " + nb_col_printed + "/" + nb_cols_in_table	+ " "  + field_name );
+					nb_col_printed++;
+				}
+				var field_value = data[field];
+
+				//console.log("     col " + nb_col_printed + "/" + nb_cols_in_table	+ " "   + field_value );
+
+				//  & 
+				var last_field =  (field_data_id == number_of_fields);
+				var more_column_to_fill = (nb_col_printed < nb_cols_in_table);
+				if (last_field && more_column_to_fill) {
+					var colspan = (nb_cols_in_table - nb_col_printed) + 1;
+					//console.log("     colspan=" + colspan);
+					html += column_start + " colspan=" + colspan;
+				} else {
+					html += column_start;
+				}
+				html += ">" + field_value + column_end;
+				nb_col_printed++;
+				field_data_id++;
+			}
+			html += "</tr>";
+		}
+		html += "</" + container + ">";
+		return html;
+	}
+
+}
